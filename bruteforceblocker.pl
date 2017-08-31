57104cd7cdd78da4fb06a404e7811b7aa7ae151a
Max Khon <fjoe@samodelkin.net>
2017-08-31 10:44:59 +0200

    Handle "error: maximum authentication attempts exceeded for ... from ..." log messages.

--- bruteforceblocker.pl
+++ bruteforceblocker.pl
@@ -81,7 +81,8 @@ while (<>) {
 	/.*Invalid user.*from ($work->{ipv4}|$work->{ipv6}|$work->{fqdn})$/i ||
 	/.*Did not receive identification string from ($work->{ipv4}|$work->{ipv6}|$work->{fqdn})$/i ||
 	/.*Bad protocol version identification .* from ($work->{ipv4}|$work->{ipv6}|$work->{fqdn})$/i ||
-	/.*User.*from ($work->{ipv4}|$work->{ipv6}|$work->{fqdn}) not allowed because.*/i ) {
+	/.*User.*from ($work->{ipv4}|$work->{ipv6}|$work->{fqdn}) not allowed because.*/i ||
+	/.*error: maximum authentication attempts exceeded for.*from ($work->{ipv4}|$work->{ipv6}|$work->{fqdn}).*/i) {
 
 	my $IP = $1;
 	if ($IP =~ /$work->{fqdn}/i) {
